ddd519cb78ab13a81776ebecb6307ce2f3712b07
Apollo84 <milanstanojevic84@gmail.com>
2022-07-29 20:30:33 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,67 @@
 let number = Math.trunc(Math.random() * 21);
 
 let numberOfGuesses = 4;
+
 document.querySelector(".numberOfGuesses").textContent = 4;
+
+let highOrLow;
+
+const displayGuess = function () {
+  document.querySelector(".displayGuess").textContent = highOrLow;
+};
+
+const changeText = function () {
+  document.querySelector(".displayGuess").style.cssText = "color: brown";
+};
+
+let decreaseAttempts = function () {
+  numberOfGuesses = numberOfGuesses - 1;
+};
 
 document.querySelector("#guessButton").addEventListener("click", function () {
   const guess = document.querySelector("#guess").value;
 
   if (!guess) {
-    document.querySelector(".displayGuess").textContent =
-      "You didn't guess anything!";
+    highOrLow = "You didn't guess anything!";
+    displayGuess();
   } else if (guess > 20) {
-    document.querySelector(".displayGuess").textContent =
-      "The number must be between 1 and 20.";
+    highOrLow = "The number must be between 1 and 20.";
+    displayGuess();
   } else if (guess > number) {
-    document.querySelector(".displayGuess").textContent = "Too high!";
-    document.querySelector(".displayGuess").style.cssText = "color: red";
-    numberOfGuesses = numberOfGuesses - 1;
+    highOrLow = "Too high!";
+    displayGuess();
+    changeText();
+    decreaseAttempts();
     document.querySelector(".numberOfGuesses").textContent = numberOfGuesses;
   } else if (guess < number) {
-    document.querySelector(".displayGuess").textContent = "Too low!";
-    document.querySelector(".displayGuess").style.cssText = "color: red";
-    numberOfGuesses = numberOfGuesses - 1;
+    highOrLow = "Too low!";
+    displayGuess();
+    changeText();
+    decreaseAttempts();
     document.querySelector(".numberOfGuesses").textContent = numberOfGuesses;
   } else {
-    document.querySelector(".displayGuess").textContent = "You got it!";
+    highOrLow = "You got it!";
+    displayGuess();
     document.querySelector(".secretNumber").textContent = number;
     document.querySelector(".body").style.cssText = "background-color: green";
-    document.querySelector(".displayGuess").style.cssText = "color: black";
+    changeText();
   }
 
   if (numberOfGuesses === 0) {
-    document.querySelector(
-      ".displayGuess"
-    ).textContent = `You lost! The number was ${number}.`;
+    highOrLow = `You lost! The number was ${number}.`;
+    displayGuess();
     document.querySelector(".body").style.cssText = "background-color: red";
-    document.querySelector(".displayGuess").style.cssText = "color: black";
+    changeText();
   }
+});
+
+//if tryagain button is clicked, reset: the secret number, the number of guesses
+document.querySelector(".againButton").addEventListener("click", function () {
+  numberOfGuesses = 4;
+  number = Math.trunc(Math.random() * 21);
+  document.querySelector(".numberOfGuesses").textContent = 4;
+  highOrLow = "The number is...";
+  displayGuess();
+  document.querySelector(".body").style.cssText = "background-color: white";
+  document.querySelector(".displayGuess").style.cssText = "color: black";
 });
